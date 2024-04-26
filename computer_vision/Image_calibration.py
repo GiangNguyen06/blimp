c246f3875c3ff3a03d6451c95768c3b0c4742348
GiangNguyen06 <76883991+GiangNguyen06@users.noreply.github.com>
2024-04-26 11:44:04 +0200

    updated image capture

--- computer_vision/Image_calibration.py
+++ computer_vision/Image_calibration.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import glob
 
@@ -9,7 +9,7 @@ chessboard_height = 5
 
 #chessboard_square_size = 20
 
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 objp = np.zeros((chessboard_width * chessboard_height, 3), np.float32)
 objp[:, :2] = np.mgrid[0:chessboard_width, 0:chessboard_height].T.reshape (-1,2) #* chessboard_square_size
@@ -19,35 +19,36 @@ objpoints = []  # 3D points in real world space
 imgpoints = []  # 2D points in image plane
 
 
-images = glob.glob('./calibration_images/*.jpg')  # Change this to the directory containing your images
+images = glob.glob('./update_calibration_images/*.jpg')  # Change this to the directory containing your images
 
 
 for fname in images:
     
   
-   img = cv2.imread(fname)
+   img = cv.imread(fname)
    
-   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+   gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 
-   ret, corners = cv2.findChessboardCorners(gray, (5,8), None)
+   ret, corners = cv.findChessboardCorners(gray, (5,8), None)
 
    if ret == True:
         objpoints.append(objp)
         
-        corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+        corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
         
         imgpoints.append(corners2)
 
 
-        cv2.drawChessboardCorners(img, (5,8), corners2, ret)
-        cv2.imshow('Calibration images', img)
-        cv2.waitKey(500)  
+        cv.drawChessboardCorners(img, (5,8), corners2, ret)
+        cv.imshow('Calibration images', img)
+        cv.waitKey(500)  
     
   
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
+print("Drawing done! Waiting for the results")
 
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 print("Calibration Matrix: ")
 print(mtx)
